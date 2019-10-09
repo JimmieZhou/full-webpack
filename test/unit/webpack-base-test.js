dc5962aadd5f0f4c9464474ec910216cf86d148f
JimmieZhou <JimmieZhou@126.com>
2019-10-09 14:18:59 +0800

    test: webpack.base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -5,7 +5,7 @@ describe('webpack.base.js test case', () => {
     const baseConfig = require('../../lib/webpack.base.js')
 
     it('entry', () => {
-        !assert.equal(baseConfig.entry.index, '/Users/jingyuan/Desktop/interview/8、webpack/geektime-webpack-course/code/chapter04/full-webpack/test/smoke/template/src/index/index.js');
-        !assert.equal(baseConfig.entry.search, '/Users/jingyuan/Desktop/interview/8、webpack/geektime-webpack-course/code/chapter04/full-webpack/test/smoke/template/src/search/index.js');
+        assert.equal(baseConfig.entry.index.indexOf('full-webpack/test/smoke/template/src/index/index.js')>-1,true);
+        assert.equal(baseConfig.entry.search.indexOf('full-webpack/test/smoke/template/src/search/index.js')>-1,true);
     });
 });
